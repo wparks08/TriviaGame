b73f37e2453c0579e9a0b4edb7d121e8329b94f3
Will Parks <wparks08@gmail.com>
2019-10-10 21:08:57 -0700

    question class added

---
+++ assets/javascript/question.js
@@ -0,0 +1,22 @@
+class Question {
+    /**
+     * 
+     * @param {string} question 
+     * @param {Array} options 
+     * @param {number} answerIndex 
+     */
+    constructor(question, options, answerIndex) {
+        this.question = question;
+        this.options = options;
+        this.optionCount = options.length;
+        this.answerIndex = answerIndex;
+    }
+
+    isAnswerCorrect(index) {
+        return index == this.answerIndex;
+    }
+
+    getOption(index) {
+        return options[index];
+    }
+}
